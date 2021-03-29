cb3b714e8907e02078cdfce77a1827b73b41a581
Gaurav Bhārat Mendse <gaurav.mendse@gmail.com>
2021-03-29 16:14:36 +0530

    replaced transaction list custom card with list tile

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -31,48 +31,33 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (ctx, index) {
                 return Card(
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          vertical: 10.0,
-                          horizontal: 15.0,
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            width: 2,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                          borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                        ),
-                        padding: EdgeInsets.all(10.0),
-                        child: Text(
-                          '₹${userTransactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w300,
-                            fontSize: 18.0,
-                            color: Theme.of(context).primaryColor,
-                          ),
+                  elevation: 3.0,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 3.0,
+                    horizontal: 5.0,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30.0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: FittedBox(
+                          child: Text(
+                              '₹${userTransactions[index].amount.toStringAsFixed(2)}'),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            userTransactions[index].title,
-                            style: Theme.of(context).textTheme.headline6,
-                          ),
-                          Text(
-                            DateFormat.yMMMd()
-                                .format(userTransactions[index].date),
-                            style: TextStyle(
-                              fontSize: 12.0,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
+                    ),
+                    title: Text(
+                      userTransactions[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(userTransactions[index].date),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete_forever),
+                      onPressed: () {},
+                    ),
                   ),
                 );
               },
